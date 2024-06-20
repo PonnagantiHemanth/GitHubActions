1894c7e7b0272897abf3b38cb02adadf9fbe665b
PonnagantiHemanth <hemanthponnaganti1@gmail.com>
2024-06-20 15:31:21 +0530

    Initial commit

--- UI_V2.py
+++ UI_V2.py
@@ -99,7 +99,7 @@ def start_test():
 
     # Create a temporary branch name based on current time
     timestamp = int(time.time())
-    branch_name = device_combobox_1.get() + device_entry_2.get() + device_combobox_3.get()
+    branch_name = device_combobox_1.get() + "/"+ device_entry_2.get() + "/" +device_combobox_3.get()
     # branch_name = device_name_values.get() + device_entry_2.get() + device_combobox_3.get()
 
     # Initialize a Git repository in the current directory
